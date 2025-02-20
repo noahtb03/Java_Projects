fb752e05abbb92e1d2b426635be80527ffc3553f
Noah Bennett <noahtimothybennett@gmail.com>
2025-02-19 19:03:10 -0500

    Update NNCalcView1.java

--- NaturalNumberCalculator/src/NNCalcView1.java
+++ NaturalNumberCalculator/src/NNCalcView1.java
@@ -1,8 +1,12 @@
 import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import components.naturalnumber.NaturalNumber;
@@ -15,197 +19,201 @@ import components.naturalnumber.NaturalNumber;
 public final class NNCalcView1 extends JFrame implements NNCalcView {
 
     /**
-     * Controller object registered with this view to observe user-interaction
-     * events.
+     * Default serial ID
      */
+    private static final long serialVersionUID = 1L;
+
+    //object for observing user interaction events
     private NNCalcController controller;
 
-    /**
-     * State of user interaction: last event "seen".
-     */
+    //state of user interation
     private enum State {
-        /**
-         * Last event was clear, enter, another operator, or digit entry, resp.
-         */
+
         SAW_CLEAR, SAW_ENTER_OR_SWAP, SAW_OTHER_OP, SAW_DIGIT
     }
 
-    /**
-     * State variable to keep track of which event happened last; needed to
-     * prepare for digit to be added to bottom operand.
-     */
+    //state variable to track last event
     private State currentState;
 
-    /**
-     * Text areas.
-     */
+    //text areas
     private final JTextArea tTop, tBottom;
 
-    /**
-     * Operator and related buttons.
-     */
+    //operator and related buttons
     private final JButton bClear, bSwap, bEnter, bAdd, bSubtract, bMultiply,
             bDivide, bPower, bRoot;
 
-    /**
-     * Digit entry buttons.
-     */
-    private final JButton[] bDigits;
+    //digit entry buttons
+    private final JButton[] bDigits = new JButton[DIGIT_BUTTONS];
 
-    /**
-     * Useful constants.
-     */
+    //useful constants
     private static final int TEXT_AREA_HEIGHT = 5, TEXT_AREA_WIDTH = 20,
             DIGIT_BUTTONS = 10, MAIN_BUTTON_PANEL_GRID_ROWS = 4,
             MAIN_BUTTON_PANEL_GRID_COLUMNS = 4, SIDE_BUTTON_PANEL_GRID_ROWS = 3,
             SIDE_BUTTON_PANEL_GRID_COLUMNS = 1, CALC_GRID_ROWS = 3,
             CALC_GRID_COLUMNS = 1;
 
-    /**
-     * No argument constructor.
-     */
+    //no argument constructor
     public NNCalcView1() {
-        // Create the JFrame being extended
 
-        /*
-         * Call the JFrame (superclass) constructor with a String parameter to
-         * name the window in its title bar
-         */
+        //Create the JFrame being extended
+        //JFrame constructor with string parameter
         super("Natural Number Calculator");
 
-        // Set up the GUI widgets --------------------------------------------
-
-        /*
-         * Set up initial state of GUI to behave like last event was "Clear";
-         * currentState is not a GUI widget per se, but is needed to process
-         * digit button events appropriately
-         */
+        //set up intitial GUI state
         this.currentState = State.SAW_CLEAR;
 
-        // TODO: fill in rest of body, following outline in comments
+        //text areas
+        this.tTop = new JTextArea("", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
+        this.tBottom = new JTextArea("", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
 
-        /*
-         * Create widgets
-         */
+        //input buttons
+        this.bClear = new JButton("Clear");
+        this.bSwap = new JButton("Swap");
+        this.bEnter = new JButton("Enter");
+        this.bAdd = new JButton("Add");
+        this.bSubtract = new JButton("Subtract");
+        this.bMultiply = new JButton("Multiply");
+        this.bDivide = new JButton("Divide");
+        this.bPower = new JButton("Power");
+        this.bRoot = new JButton("Root");
 
-        // Set up the GUI widgets --------------------------------------------
+        //number buttons
+        this.bDigits[0] = new JButton("0");
+        this.bDigits[1] = new JButton("1");
+        this.bDigits[2] = new JButton("2");
+        this.bDigits[3] = new JButton("3");
+        this.bDigits[4] = new JButton("4");
+        this.bDigits[5] = new JButton("5");
+        this.bDigits[6] = new JButton("6");
+        this.bDigits[7] = new JButton("7");
+        this.bDigits[8] = new JButton("8");
+        this.bDigits[9] = new JButton("9");
 
-        /*
-         * Text areas should wrap lines, and should be read-only; they cannot be
-         * edited because allowing keyboard entry would require checking whether
-         * entries are digits, which we don't want to have to do
-         */
+        //set text area settings
+        this.tTop.setEditable(false);
+        this.tTop.setLineWrap(true);
+        this.tTop.setWrapStyleWord(true);
+        this.tBottom.setEditable(false);
+        this.tBottom.setLineWrap(true);
+        this.tBottom.setWrapStyleWord(true);
 
-        /*
-         * Initially, the following buttons should be disabled: divide (divisor
-         * must not be 0) and root (root must be at least 2) -- hint: see the
-         * JButton method setEnabled
-         */
+        //initially disable these buttons
+        this.bDivide.setEnabled(false);
+        this.bRoot.setEnabled(false);
 
-        /*
-         * Create scroll panes for the text areas in case number is long enough
-         * to require scrolling
-         */
+        //create scroll panes
+        JScrollPane tTopScrollPane = new JScrollPane(this.tTop);
+        JScrollPane tBottomScrollPane = new JScrollPane(this.tBottom);
 
-        /*
-         * Create main button panel
-         */
+        //main buttone panel
+        JPanel mainButtonPanel = new JPanel(new GridLayout(
+                MAIN_BUTTON_PANEL_GRID_ROWS, MAIN_BUTTON_PANEL_GRID_COLUMNS));
 
-        /*
-         * Add the buttons to the main button panel, from left to right and top
-         * to bottom
-         */
+        //add buttons to the main button panel, left to right, top to bottom
+        mainButtonPanel.add(this.bDigits[7]);
+        mainButtonPanel.add(this.bDigits[8]);
+        mainButtonPanel.add(this.bDigits[9]);
+        mainButtonPanel.add(this.bAdd);
+        mainButtonPanel.add(this.bDigits[4]);
+        mainButtonPanel.add(this.bDigits[5]);
+        mainButtonPanel.add(this.bDigits[6]);
+        mainButtonPanel.add(this.bSubtract);
+        mainButtonPanel.add(this.bDigits[1]);
+        mainButtonPanel.add(this.bDigits[2]);
+        mainButtonPanel.add(this.bDigits[3]);
+        mainButtonPanel.add(this.bMultiply);
+        mainButtonPanel.add(this.bDigits[0]);
+        mainButtonPanel.add(this.bPower);
+        mainButtonPanel.add(this.bRoot);
+        mainButtonPanel.add(this.bDivide);
 
-        /*
-         * Create side button panel
-         */
+        //side button panel
+        JPanel sideButtonPanel = new JPanel(new GridLayout(
+                SIDE_BUTTON_PANEL_GRID_ROWS, SIDE_BUTTON_PANEL_GRID_COLUMNS));
 
-        /*
-         * Add the buttons to the side button panel, from left to right and top
-         * to bottom
-         */
+        //add buttons to side panel, left to right, top to bottom
+        sideButtonPanel.add(this.bClear);
+        sideButtonPanel.add(this.bSwap);
+        sideButtonPanel.add(this.bEnter);
 
-        /*
-         * Create combined button panel organized using flow layout, which is
-         * simple and does the right thing: sizes of nested panels are natural,
-         * not necessarily equal as with grid layout
-         */
+        //create button panel to hold all buttons
+        JPanel combinedButtonPanel = new JPanel(new FlowLayout());
 
-        /*
-         * Add the other two button panels to the combined button panel
-         */
+        //nest other button panels in the combined one
+        combinedButtonPanel.add(mainButtonPanel);
+        combinedButtonPanel.add(sideButtonPanel);
 
-        /*
-         * Organize main window
-         */
+        //create layout
+        this.setLayout(new GridLayout(CALC_GRID_ROWS, CALC_GRID_COLUMNS));
 
-        /*
-         * Add scroll panes and button panel to main window, from left to right
-         * and top to bottom
-         */
+        //add scroll panes to button panel
+        this.add(tTopScrollPane);
+        this.add(tBottomScrollPane);
+        this.add(combinedButtonPanel);
 
-        // Set up the observers ----------------------------------------------
+        //register this as observer for all GUI events
+        this.bDigits[0].addActionListener(this);
+        this.bDigits[1].addActionListener(this);
+        this.bDigits[2].addActionListener(this);
+        this.bDigits[3].addActionListener(this);
+        this.bDigits[4].addActionListener(this);
+        this.bDigits[5].addActionListener(this);
+        this.bDigits[6].addActionListener(this);
+        this.bDigits[7].addActionListener(this);
+        this.bDigits[8].addActionListener(this);
+        this.bDigits[9].addActionListener(this);
+        this.bAdd.addActionListener(this);
+        this.bSubtract.addActionListener(this);
+        this.bMultiply.addActionListener(this);
+        this.bDivide.addActionListener(this);
+        this.bEnter.addActionListener(this);
+        this.bSwap.addActionListener(this);
+        this.bClear.addActionListener(this);
+        this.bPower.addActionListener(this);
+        this.bRoot.addActionListener(this);
 
-        /*
-         * Register this object as the observer for all GUI events
-         */
-
-        // Set up the main application window --------------------------------
-
-        /*
-         * Make sure the main window is appropriately sized, exits this program
-         * on close, and becomes visible to the user
-         */
+        //size main window, exit it on close, and is visible to user
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
 
     }
 
     @Override
     public void registerObserver(NNCalcController controller) {
-
-        // TODO: fill in body
-
+        this.controller = controller;
     }
 
     @Override
     public void updateTopDisplay(NaturalNumber n) {
-
-        // TODO: fill in body
-
+        String num = n.toString();
+        this.tTop.setText(num);
     }
 
     @Override
     public void updateBottomDisplay(NaturalNumber n) {
-
-        // TODO: fill in body
-
+        String num = n.toString();
+        this.tBottom.setText(num);
     }
 
     @Override
     public void updateSubtractAllowed(boolean allowed) {
-
-        // TODO: fill in body
-
+        this.bSubtract.setEnabled(allowed);
     }
 
     @Override
     public void updateDivideAllowed(boolean allowed) {
-
-        // TODO: fill in body
-
+        this.bDivide.setEnabled(allowed);
     }
 
     @Override
     public void updatePowerAllowed(boolean allowed) {
-
-        // TODO: fill in body
-
+        this.bPower.setEnabled(allowed);
     }
 
     @Override
     public void updateRootAllowed(boolean allowed) {
-
-        // TODO: fill in body
-
+        this.bRoot.setEnabled(allowed);
     }
 
     @Override
@@ -272,10 +280,9 @@ public final class NNCalcView1 extends JFrame implements NNCalcView {
                 }
             }
         }
-        /*
-         * Set the cursor back to normal (because we changed it at the beginning
-         * of the method body)
-         */
+
+        //Set the cursor back to normal (because we changed it at the beginning
+        //of the method body)
         this.setCursor(Cursor.getDefaultCursor());
     }
 
